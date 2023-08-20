ecf23643345d342fcc8c01e0db4e7f682e6dabae
Estu Ramadian Wiharto <weramadian@gmail.com>
2023-08-20 17:18:27 +0700

    feat: add useTitleCase hook

--- src/feature/pokedex/utils/hooks.js
+++ src/feature/pokedex/utils/hooks.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { pokemonTypeList } from './data';
 
-const usePokemonTypeData = (key) => {
+export const usePokemonTypeData = (key) => {
   const [typeInfo, setTypeInfo] = useState(null);
 
   useEffect(() => {
@@ -15,6 +15,18 @@ const usePokemonTypeData = (key) => {
   return typeInfo;
 };
 
-export {
-  usePokemonTypeData
-}
+export const useTitleCase = (initialText = '') => {
+  const [text, setText] = useState(initialText);
+
+  useEffect(() => {
+    function toTitleCase(str) {
+      return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
+    }
+
+    setText(toTitleCase(initialText));
+  }, [initialText]);
+
+  return text;
+};
