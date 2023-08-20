3825092e24378527265b671247ee003eb454d15e
Estu Ramadian Wiharto <weramadian@gmail.com>
2023-08-20 20:26:45 +0700

    fix: stats component adjustment based on pokeapi

--- src/feature/pokedex/components/Stats.jsx
+++ src/feature/pokedex/components/Stats.jsx
@@ -1,86 +1,73 @@
 import styled from '@emotion/styled'
 import { Box, Button, Paper, Stack, Typography } from '@mui/material'
 import TypeBadge from './TypeBadge'
+import PropTypes from 'prop-types';
+import { useFindPokemonId, useTitleCase } from '../utils/hooks';
+import { statTitle } from '../utils/data';
 
-const statTitle = [
-  'HP',
-  'Attack',
-  'Defense',
-  'Sp. Attack',
-  'Sp. Defense',
-  'Speed'
-]
+export default function Stats({
+  selectedPokemon
+}) {
 
-export default function Stats() {
+  const pokemonName = useTitleCase(selectedPokemon?.name);
+  const pokemonId = useFindPokemonId(selectedPokemon?.id);
+
+  const statData = selectedPokemon?.stats?.reduce((result, stat) => {
+    result[stat.stat.name] = stat.base_stat;
+    return result;
+  }, {});
+  
+
   return (
     <StyledPaper>
-      <Stack spacing={2}>
-        <Stack
-          alignItems='center'
-          direction='row'
-          justifyContent='space-between'
-        >
-          <Typography 
-            sx={{fontWeight: 600}}
-            variant='h6'
-          >Bulbasaur</Typography>
-          <Typography 
-            color='#B3B3B3'
-            sx={{fontWeight: 600}}
-            variant='h6'
-          >#0001</Typography>
-        </Stack>
+      <Stack spacing={4}>
         <Box>
-          <Typography 
-            sx={{fontWeight: 600}}
-            variant='h6'
-          >Type</Typography>
+          <Stack
+            alignItems='center'
+            direction='row'
+            justifyContent='space-between'
+          >
+            <Typography 
+              sx={{fontWeight: 600}}
+              variant='h3'
+            >{pokemonName}</Typography>
+            <Typography 
+              color='#B3B3B3'
+              sx={{fontWeight: 600}}
+              variant='h3'
+            >{pokemonId}</Typography>
+          </Stack>
           <Stack 
             direction='row'
             alignItems='center'
-            sx={{gap: 1}}
+            justifyContent='flex-start'
+            spacing={1}
           >
             {
-              Array.from({length: 2}).map((_, index) => (
-                <TypeBadge key={index} />
+              selectedPokemon?.types?.map((type, index) => (
+                <TypeBadge 
+                  key={index} 
+                  typeName={type?.type?.name}
+                />
               ))
             }
           </Stack>
         </Box>
         <Box>
-          <Typography 
-            sx={{fontWeight: 600}}
-            variant='h6'
-          >Weakness</Typography>
-          <Stack 
-            direction='row'
-            alignItems='center'
-            flexWrap='wrap'
-            sx={{gap: 1}}
-          >
+          <Stack direction='row' spacing={2} flexWrap='wrap' justifyContent='space-between'>
+            <Stack sx={{width: '40%', marginLeft: '16px !important'}} alignItems='flex-end'>
+              <Typography variant='body1' color='#B3B3B3'>Height</Typography>
+              <Typography variant='h3'>{selectedPokemon?.height*10} cm</Typography>
+            </Stack>
+            <Stack sx={{width: '40%'}} alignItems='flex-start'>
+              <Typography variant='body1' color='#B3B3B3'>Weight</Typography>
+              <Typography variant='h3'>{selectedPokemon?.weight*0.1} kg</Typography>
+            </Stack>
             {
-              Array.from({length: 5}).map((_, index) => (
-                <TypeBadge key={index} />
-              ))
-            }
-          </Stack>
-        </Box>
-        <Box>
-          <Typography 
-            sx={{fontWeight: 600}}
-            variant='h6'
-          >Stats</Typography>
-          <Stack spacing={1}>
-            {
-              statTitle.map((item) => (
-                <Stack 
-                  key={item}
-                  direction='row'
-                  alignItems='center'
-                  justifyContent='space-between'
-                >
-                  <Typography variant='body1'>{item}</Typography>
-                  <RenderStatBar />
+              statTitle.map((item, index) => (
+                <Stack sx={{width: '40%'}} alignItems={index%2 === 0 ? 'flex-end' : 'flex-start'} key={item.key}>
+                  <Typography variant='body1' color='#B3B3B3'>{item.label}</Typography>
+                  <Typography variant='h3'>{statData?.[item.key]}</Typography>
                 </Stack>
               ))
             }
@@ -90,7 +77,7 @@ export default function Stats() {
           direction='row'
           justifyContent='flex-end'
         >
-          <Button variant='contained'>
+          <Button variant='text'>
             Details
           </Button>
         </Stack>
@@ -99,21 +86,11 @@ export default function Stats() {
   )
 }
 
+Stats.propTypes = {
+  selectedPokemon: PropTypes.object,
+};
+
 const StyledPaper = styled(Paper)`
   padding: 24px;
   border-radius: 1.5rem;
 `
-
-const AbilityBar = styled(Box)`
-  width: ${(_) => _.barWidth ? _.barWidth : '300px'};
-  height: 20px;
-  background-color: ${(_) => _.barColor ? _.barColor : '#DEDEDE'};
-`
-
-const RenderStatBar = () => {
-  return (
-    <AbilityBar>
-      <AbilityBar  barColor='#9BCC50' barWidth='100px' />
-    </AbilityBar>
-  )
-}
